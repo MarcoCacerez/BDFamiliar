3e5103aa45fe7dc9fcf588729bb0d3af755d8de6
Marco Aldair de Jesus Caceres <89954414+MarcoCacerez@users.noreply.github.com>
2022-06-14 14:19:28 -0500

    Add files via upload

---
+++ BDFamiliar.pl
@@ -0,0 +1,48 @@
+familia(
+    persona(
+        [tomas,garcia,perez],
+        fecha(7,mayo,1960),
+        trabajo(profesor,60)
+    ),
+    persona([ana,lopez,ruiz],
+            fecha(10,marzo,1962),
+            trabajo(medica,90)),
+    [
+        persona([juan,garcia,lopez],
+                fecha(5,enero,1990),
+                estudiante),
+        persona([maria,garcia,lopez],
+                fecha(12,abril,1992),
+                estudiante)
+    ]
+).
+
+familia(
+    persona(
+        [jose,perez,ruiz],
+        fecha(6,marzo,1963),
+        trabajo(pintor,120)
+    ),
+    persona(
+        [luisa,galvez,perez],
+        fecha(12,mayo,1964),
+        trabajo(medica,90)
+    ),
+    [
+        persona(
+            [juan_luis,perez,perez],
+            fecha(5,febrero,1990),
+            estudiante
+        ),
+        persona(
+            [maria_jose,perez,perez],
+            fecha(12,junio,1992),
+            estudiante
+        ),
+        persona(
+            [jose_maria,perez, perez],
+            fecha(12,julio,1994),
+            estudiante
+        )
+    ]
+).
